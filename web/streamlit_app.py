82479923fa89c01857eeaed29943b56a780113ba
codeotter0201 <codeotter0201@gmail.com>
2023-01-28 12:01:59 +0800

    format code style by black.

--- web/streamlit_app.py
+++ web/streamlit_app.py
@@ -4,26 +4,31 @@ import streamlit as st
 from worker import Worker
 import plotly.graph_objects as go
 
-def plot_bar_line_chart(line_data:pd.Series, bar_data:pd.Series):
+
+def plot_bar_line_chart(line_data: pd.Series, bar_data: pd.Series):
     fig = go.Figure()
     fig.add_trace(
         go.Scatter(
             x=line_data.index,
             y=line_data,
-            name='Cumulative',
-        ))
+            name="Cumulative",
+        )
+    )
 
     fig.add_trace(
         go.Bar(
             x=line_data.index,
             y=bar_data,
-            name='Monthly Profit',
-        ))
+            name="Monthly Profit",
+        )
+    )
     return fig
+
 
 @st.cache
 def get_worker():
     return Worker(1)
+
 
 @st.experimental_singleton
 def figs():
@@ -31,18 +36,22 @@ def figs():
     name_list = list(get_worker().get_trades().sn.unique())
     for sn in name_list[:]:
         temp_fig[sn] = {}
-        temp_fig[sn]['pnl_kbar'], temp_fig[sn]['mafe'] = get_worker().plot_strategy_figs(sn)
+        (
+            temp_fig[sn]["pnl_kbar"],
+            temp_fig[sn]["mafe"],
+        ) = get_worker().plot_strategy_figs(sn)
     return temp_fig
 
 
 ###############################################################################################################################################################################
 
-def portfolio_summary(symbol:str, trades:pd.DataFrame, trades_less:pd.DataFrame):
-    if (symbol == '') | (symbol == 'end'):
+
+def portfolio_summary(symbol: str, trades: pd.DataFrame, trades_less: pd.DataFrame):
+    if (symbol == "") | (symbol == "end"):
         return
 
     # mode_list = ['Online', 'Testing', 'ALL']
-    
+
     # for mode, col in zip(mode_list, st.columns([1 for i in mode_list])):
     #     with col:
     #         if mode == 'Online':
@@ -66,11 +75,29 @@ def portfolio_summary(symbol:str, trades:pd.DataFrame, trades_less:pd.DataFrame)
     #     trades = get_worker().get_trades().query(f'`symbol` == @symbol').drop(['symbol'], axis=1).reset_index(drop=True)[::-1]
     #     trades_less = get_worker().get_trades_less().query(f'`symbol` == @symbol').drop(['symbol'], axis=1).reset_index(drop=True)[::-1]
 
-    trades = trades.query(f'`symbol` == @symbol').drop(['symbol'], axis=1).reset_index(drop=True)[::-1]
-    trades_less = trades_less.query(f'`symbol` == @symbol').drop(['symbol'], axis=1).reset_index(drop=True)[::-1]
+    trades = (
+        trades.query(f"`symbol` == @symbol")
+        .drop(["symbol"], axis=1)
+        .reset_index(drop=True)[::-1]
+    )
+    trades_less = (
+        trades_less.query(f"`symbol` == @symbol")
+        .drop(["symbol"], axis=1)
+        .reset_index(drop=True)[::-1]
+    )
 
-    plot_strategies = list(trades.groupby('sn').apply(lambda x:x.g_mfe.sum() / x.mae.sum()).sort_values()[::-1].index)
-    temp = list(trades.groupby('sn').apply(lambda x:x.g_mfe.sum() / x.mae.sum()).sort_values()[::-1].index)
+    plot_strategies = list(
+        trades.groupby("sn")
+        .apply(lambda x: x.g_mfe.sum() / x.mae.sum())
+        .sort_values()[::-1]
+        .index
+    )
+    temp = list(
+        trades.groupby("sn")
+        .apply(lambda x: x.g_mfe.sum() / x.mae.sum())
+        .sort_values()[::-1]
+        .index
+    )
 
     for sn, col in zip(plot_strategies, st.columns([1 for _ in plot_strategies])):
         with col:
@@ -78,85 +105,152 @@ def portfolio_summary(symbol:str, trades:pd.DataFrame, trades_less:pd.DataFrame)
                 temp.remove(sn)
 
     if len(temp) == 0:
-        st.markdown('### Please select a strategy above.👆')
+        st.markdown("### Please select a strategy above.👆")
         return
     else:
-        trades = trades[trades['sn'].isin(temp)]
-        trades_less = trades_less[trades_less['sn'].isin(temp)]
+        trades = trades[trades["sn"].isin(temp)]
+        trades_less = trades_less[trades_less["sn"].isin(temp)]
 
-    pnl, pct = trades.set_index('exit_time')['pnl'], trades.set_index('exit_time')['pct']
+    pnl, pct = (
+        trades.set_index("exit_time")["pnl"],
+        trades.set_index("exit_time")["pct"],
+    )
     tab1, tab2 = st.tabs(["🙏Performance", "🎈Trades"])
     with tab1:
         col1, col2 = st.columns([3, 1])
         with col1:
-            month = [f'{y}-{m}' for y, m in zip(pnl.index.year, pnl.index.month)]
+            month = [f"{y}-{m}" for y, m in zip(pnl.index.year, pnl.index.month)]
             pnl = pnl.groupby(month).sum()
             pnl.index = pd.to_datetime(pnl.index)
             pnl = pnl.sort_index()
             fig3 = plot_bar_line_chart(line_data=pnl.cumsum(), bar_data=pnl)
-            fig3.update_layout(title='Monthly Return[Point]', title_x=0.5)
+            fig3.update_layout(title="Monthly Return[Point]", title_x=0.5)
             st.plotly_chart(fig3, use_container_width=True)
         with col2:
             performance = dict(
                 start_date=trades.entry_time.iloc[-1],
-                monthly_avg_trades = trades.groupby([trades.entry_time.dt.month, trades.entry_time.dt.year]).size().mean(),
-                monthly_avg_profit = trades.groupby([trades.exit_time.dt.month, trades.exit_time.dt.year]).pnl.sum().mean(),
-                long_strategies = len([i for i in trades.sn.unique() if 'LONG' in i]),
-                short_strategies = len([i for i in trades.sn.unique() if 'LONG' not in i]),
+                monthly_avg_trades=trades.groupby(
+                    [trades.entry_time.dt.month, trades.entry_time.dt.year]
+                )
+                .size()
+                .mean(),
+                monthly_avg_profit=trades.groupby(
+                    [trades.exit_time.dt.month, trades.exit_time.dt.year]
+                )
+                .pnl.sum()
+                .mean(),
+                long_strategies=len([i for i in trades.sn.unique() if "LONG" in i]),
+                short_strategies=len(
+                    [i for i in trades.sn.unique() if "LONG" not in i]
+                ),
             )
-            st.dataframe(pd.Series(performance, name=str(symbol)), use_container_width=True, width=200)
+            st.dataframe(
+                pd.Series(performance, name=str(symbol)),
+                use_container_width=True,
+                width=200,
+            )
     with tab2:
-        pw = st.text_input('space', label_visibility='hidden')
-        if str(pw) == 'otter':
-            trades = trades.set_index('sn').style.format(subset=['pnl', 'entry_price', 'exit_price', 'mae', 'mae_lv1', 'mfe', 'g_mfe', 'h2c', 'l2c', 'pct'], formatter="{:.2f}")
+        pw = st.text_input("space", label_visibility="hidden")
+        if str(pw) == "otter":
+            trades = trades.set_index("sn").style.format(
+                subset=[
+                    "pnl",
+                    "entry_price",
+                    "exit_price",
+                    "mae",
+                    "mae_lv1",
+                    "mfe",
+                    "g_mfe",
+                    "h2c",
+                    "l2c",
+                    "pct",
+                ],
+                formatter="{:.2f}",
+            )
             st.dataframe(trades, use_container_width=True)
         else:
-            trades = trades_less.set_index('sn').style.format(subset=['pnl', 'entry_price', 'exit_price', 'pct'], formatter="{:.2f}")
+            trades = trades_less.set_index("sn").style.format(
+                subset=["pnl", "entry_price", "exit_price", "pct"], formatter="{:.2f}"
+            )
             st.dataframe(trades, use_container_width=True)
-
 
 
 ###############################################################################################################################################################################
 
-def strategy_summary(sn:str, trades:pd.DataFrame, trades_less:pd.DataFrame, performance:pd.DataFrame):
-    if (sn == ' ') | (sn == 'end '):
+
+def strategy_summary(
+    sn: str, trades: pd.DataFrame, trades_less: pd.DataFrame, performance: pd.DataFrame
+):
+    if (sn == " ") | (sn == "end "):
         return
-    fig, fig2 = figs()[sn]['pnl_kbar'], figs()[sn]['mafe']
-    trades = trades.query(f'`sn` == @sn').drop(['sn'], axis=1).reset_index(drop=True)[::-1]
-    trades_less = trades_less.query(f'`sn` == @sn').drop(['sn'], axis=1).reset_index(drop=True)[::-1]
+    fig, fig2 = figs()[sn]["pnl_kbar"], figs()[sn]["mafe"]
+    trades = (
+        trades.query(f"`sn` == @sn").drop(["sn"], axis=1).reset_index(drop=True)[::-1]
+    )
+    trades_less = (
+        trades_less.query(f"`sn` == @sn")
+        .drop(["sn"], axis=1)
+        .reset_index(drop=True)[::-1]
+    )
     symbol = trades.symbol.unique()
-    pnl, pct = trades.set_index('exit_time')['pnl'], trades.set_index('exit_time')['pct']
+    pnl, pct = (
+        trades.set_index("exit_time")["pnl"],
+        trades.set_index("exit_time")["pct"],
+    )
     performance = performance.loc[sn]
-    performance['point_return'] = pnl.sum()
-    performance['percent_return'] = pct.sum()
-    tab1, tab2, tab3, tab4 = st.tabs(["💵Kbar PnL", "📊MAE MFE", "🙏Performance", "🎈Trades"])
+    performance["point_return"] = pnl.sum()
+    performance["percent_return"] = pct.sum()
+    tab1, tab2, tab3, tab4 = st.tabs(
+        ["💵Kbar PnL", "📊MAE MFE", "🙏Performance", "🎈Trades"]
+    )
     with tab1:
-        fig.update_layout(height=450, width=700, title='')
+        fig.update_layout(height=450, width=700, title="")
         st.plotly_chart(fig, use_container_width=True)
     with tab2:
-        fig2.update_layout(height=1800, width=700, title='')
+        fig2.update_layout(height=1800, width=700, title="")
         st.plotly_chart(fig2, use_container_width=True)
     with tab3:
         col1, col2 = st.columns([3, 1])
         with col1:
-            month = [f'{y}-{m}' for y, m in zip(pnl.index.year, pnl.index.month)]
+            month = [f"{y}-{m}" for y, m in zip(pnl.index.year, pnl.index.month)]
             pnl = pnl.groupby(month).sum()
             pnl.index = pd.to_datetime(pnl.index)
             pnl = pnl.sort_index()
             fig3 = plot_bar_line_chart(line_data=pnl.cumsum(), bar_data=pnl)
-            fig3.update_layout(title='Monthly Return[Point]', title_x=0.5)
+            fig3.update_layout(title="Monthly Return[Point]", title_x=0.5)
             st.plotly_chart(fig3, use_container_width=True)
         with col2:
-            st.dataframe(pd.Series(performance.to_dict(), name=str(symbol)), use_container_width=True, width=200)
+            st.dataframe(
+                pd.Series(performance.to_dict(), name=str(symbol)),
+                use_container_width=True,
+                width=200,
+            )
 
     with tab4:
-        pw = st.text_input('space2', label_visibility='hidden')
-        if str(pw) == 'otter':
-            trades = trades.style.format(subset=['pnl', 'entry_price', 'exit_price', 'mae', 'mae_lv1', 'mfe', 'g_mfe', 'h2c', 'l2c', 'pct'], formatter="{:.2f}")
+        pw = st.text_input("space2", label_visibility="hidden")
+        if str(pw) == "otter":
+            trades = trades.style.format(
+                subset=[
+                    "pnl",
+                    "entry_price",
+                    "exit_price",
+                    "mae",
+                    "mae_lv1",
+                    "mfe",
+                    "g_mfe",
+                    "h2c",
+                    "l2c",
+                    "pct",
+                ],
+                formatter="{:.2f}",
+            )
             st.dataframe(trades, use_container_width=True)
         else:
-            trades = trades_less.style.format(subset=['pnl', 'entry_price', 'exit_price', 'pct'], formatter="{:.2f}")
+            trades = trades_less.style.format(
+                subset=["pnl", "entry_price", "exit_price", "pct"], formatter="{:.2f}"
+            )
             st.dataframe(trades, use_container_width=True)
+
 
 ###############################################################################################################################################################################
 
@@ -173,7 +267,6 @@ st.set_page_config(
 )
 
 st.title("Welcome to Fincode! 🎉")
-
 
 
 about_tag, portfolio_tag, strategy_tag = st.tabs(["😎About", "📈Portfolio", "🎯Strategy"])
@@ -233,13 +326,8 @@ with portfolio_tag:
     #             ret = symbol
 
     symbols = sorted(trades.symbol.unique())
-    symbols = [''] + list(symbols) + ['end']
-    ret = st.select_slider(
-        '',
-        symbols,
-        value=symbols[1],
-        help='\n\n'.join(symbols)
-    )
+    symbols = [""] + list(symbols) + ["end"]
+    ret = st.select_slider("", symbols, value=symbols[1], help="\n\n".join(symbols))
 
     if len(symbols) != 0:
         portfolio_summary(ret, trades, trades_less)
